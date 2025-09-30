dc281a7c290435c75a6b470e90f84168abb603c9
igor Silva <82171880+igorrzinho@users.noreply.github.com>
2025-09-30 19:04:54 +0000

    modificando Peca.cs para parar de dar aviso

--- JogoXadrez/tabuleiro/Peca.cs
+++ JogoXadrez/tabuleiro/Peca.cs
@@ -2,7 +2,7 @@ namespace tabuleiro
 {
     class Peca
     {
-        public Posicao posicao { get; set; }
+        public Posicao? posicao { get; set; }
         public Cor cor { get; set; }
         public int qtdMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
