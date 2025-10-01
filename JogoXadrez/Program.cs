9421b63b814467644a1aa085fee8b3f9f8129f39
igor Silva <82171880+igorrzinho@users.noreply.github.com>
2025-10-01 14:17:48 +0000

    implementando as restrições de movimento

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -12,21 +12,33 @@ namespace JogoXadrez
                 PartidaDeXadrez partida = new PartidaDeXadrez();
                 while (!partida.terminada)
                 {
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab);
+                    try
+                    {
 
-                    Console.Write("Origem? ");
-                    Posicao origem = Tela.lerPosicaoXadrez().ToPosicao();
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab);
+                        Console.WriteLine("Turno: " + partida.turno);
+                        Console.WriteLine("Aguardando Jogador: " + partida.jogadorAtual);
+                        Console.Write("Origem? ");
+                        Posicao origem = Tela.lerPosicaoXadrez().ToPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
+                        
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
-                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+                        Console.Write("Destino? ");
+                        Posicao destino = Tela.lerPosicaoXadrez().ToPosicao();
+                        partida.validarPosicaoDestino(origem, destino);
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e)
+                    {
+                        System.Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
 
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
-                    Console.Write("Destino? ");
-                    Posicao destino = Tela.lerPosicaoXadrez().ToPosicao();
-
-                    partida.execultaMovimento(origem, destino);
                 }
 
             }
--- JogoXadrez/xadrez/PatidaDeXadrez.cs
+++ JogoXadrez/xadrez/PatidaDeXadrez.cs
@@ -5,8 +5,8 @@ namespace xadrez
     class PartidaDeXadrez
     {
         public Tabuleiro tab { get; private set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual{ get; private set; }
         public bool terminada {get; private set;}
 
 
@@ -27,6 +27,48 @@ namespace xadrez
             tab.colocarPeca(p, destino);
         }
 
+        public void realizaJogada(Posicao origem, Posicao destino)
+        {
+            execultaMovimento(origem, destino);
+            turno++;
+            mudaJogador();
+        }
+        public void validarPosicaoDeOrigem(Posicao pos)
+        {
+            if (tab.peca(pos) == null)
+            {
+                throw new TabuleiroException("Não existe peça na posição nesta origem ");
+            }
+
+            if (jogadorAtual != tab.peca(pos).cor)
+            {
+                throw new TabuleiroException("Vez do jogador : " + jogadorAtual);
+            }
+            if (!tab.peca(pos).existeMovimentosPossiveis())
+            {
+                throw new TabuleiroException("Não existe movimentos possiveis para a  peça!");
+                
+            }
+        }
+        public void validarPosicaoDestino(Posicao origem, Posicao destino)
+        {
+            if (!tab.peca(origem).podeMoverPara(destino))
+            {
+                throw new TabuleiroException("Posiçao de destino invalida");
+            }
+        }
+
+        private void mudaJogador()
+        {
+            if (jogadorAtual == Cor.Branca)
+            {
+                jogadorAtual = Cor.Preta;
+            }
+            else
+            {
+                jogadorAtual = Cor.Branca;
+            }
+        }
         private void colocarPecas()
         {
             // pecas brancas
